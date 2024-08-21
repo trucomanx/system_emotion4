a79180ca9226b07ece11fd425fd860d956d23bdb
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-08-21 13:29:03 -0300

    ordenando

--- example/example.py
+++ example/example.py
@@ -1,27 +1,54 @@
 #!/usr/bin/python3
 
-
 import os
+
+## Because the KERAS used is a old verion, KERAS 1
 os.environ['TF_USE_LEGACY_KERAS'] = '1'
 
+## Allows memory allocation on the GPU to be done asynchronously, 
+## potentially improving performance and efficiency in certain scenarios.
+os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
+
+## Specify which GPUs are available to a program.
+## The list of GPUs is usually represented by indices starting at 0.
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # disable cuda
+
+import sys
+sys.path.append('../library') 
+
+
 import SystemEmotion4Lib.Classifier as sec
+import numpy as np
 from PIL import Image
 
-cls=sec.Emotion4Classifier();
+cls=sec.Emotion4Classifier( checkpoint='shufflenetv2k16',
+                            model_type_face='efficientnet_b3',
+                            model_type_body='efficientnet_b3',
+                            model_type_skel=20,
+                            model_type_skel_enable_minus=True,
+                            model_type_fusion=7);
+
 
 labels   = ['negative','neutral','pain','positive'];
-filepath = 'dataset/positive.png';
+filepaths = [   'KlingAi/negative.png',
+                'KlingAi/neutral.png',
+                'KlingAi/pain.png',
+                'KlingAi/positive.png',
+                'KlingAi/pain_negative.png'];
 
-img_pil = Image.open(filepath)
+for filepath in filepaths:
+    print("\n")
+    
+    img_pil = Image.open(filepath)
 
-res=cls.from_img_pil(img_pil);
+    res=cls.from_img_pil(img_pil);
 
-print(filepath,labels[res]);
+    print(filepath,labels[res]);
 
-import numpy as np
-
-res=cls.get_input_fusion_from_pil(img_pil);
-print(res)
+    res=cls.get_input_fusion_from_pil(img_pil);
+    print('face',res[0])
+    print('body',res[1])
+    #print('skel',res[2])
 
 
 
--- library/SystemEmotion4Lib/Classifier.py
+++ library/SystemEmotion4Lib/Classifier.py
@@ -9,6 +9,8 @@ import BodyEmotion4Lib.Classifier     as bec
 import SkeletonEmotion4Lib.Classifier as sec
 import FusionEmotion4Lib.Classifier   as fsc
 
+import tensorflow as tf
+import numpy as np
 
 class Emotion4Classifier:
     """Class to classify 4 body languages.
@@ -26,7 +28,7 @@ class Emotion4Classifier:
                     model_type_face='efficientnet_b3',
                     model_type_body='efficientnet_b3',
                     model_type_skel=20,
-                    model_type_skel_minus=None,
+                    model_type_skel_enable_minus=False,
                     model_type_fusion=11):
         """Inicializer of class Emotion4Classifier.
         
@@ -34,16 +36,24 @@ class Emotion4Classifier:
             param file_of_weight: Archivo donde se encuentran los pesos.
         """
         
-        self.det=opp.Detector(checkpoint='shufflenetv2k16');
+
 
         self.cls_face=fec.FaceEmotion4Classifier(model_type=model_type_face);
 
         self.cls_body=bec.Emotion4Classifier(model_type=model_type_body);
 
-        self.cls_skel=sec.Emotion4Classifier(ncod=model_type_skel, minus=model_type_skel_minus);
+        self.cls_skel=sec.Emotion4Classifier(ncod=model_type_skel);
+        
+        #mejor esa orden para que cargue al final
+        self.det=opp.Detector(checkpoint='shufflenetv2k16');
 
-        self.cls_fusion=fsc.Emotion4Classifier(ncod=model_type_fusion, minus=model_type_skel_minus);
-
+        if model_type_skel_enable_minus==True:
+            self.cls_fusion=fsc.Emotion4Classifier(ncod=model_type_fusion, skel_size=model_type_skel);
+        else:
+            self.cls_fusion=fsc.Emotion4Classifier(ncod=model_type_fusion, skel_size=None);
+        
+        self.enable_minus=model_type_skel_enable_minus;
+        
     def get_input_fusion_from_pil(self,pil_img):
         """Classify a body language data from a numpy vector object with N elements 
         
@@ -67,7 +77,10 @@ class Emotion4Classifier:
         if face_roi is not None:
             res_face = self.cls_face.predict_pil(face_roi);
         
-        res_skel = self.cls_skel.predict_vec(skel_vec);
+        if self.enable_minus==True:
+            res_skel = self.cls_skel.predict_minus_vec(skel_vec);
+        else:
+            res_skel = self.cls_skel.predict_vec(skel_vec);
         
         return res_face, res_body, res_skel;
 
@@ -108,4 +121,21 @@ class Emotion4Classifier:
         """
         return ['negative','neutro','pain','positive'];
 
+    def dummy_test(self):
+        # Definindo o modelo
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(1, input_shape=(4,))
+        ]);
 
+        # Compilando o modelo (opcional neste caso, pois não vamos treinar)
+        model.compile(optimizer='adam', loss='mse');
+
+        # Criando um vetor de entrada com 4 elementos
+        input_data = np.array([[0.5, 0.3, 0.7, 0.2]]);
+
+        # Fazendo uma previsão com o modelo
+        prediction = model.predict(input_data);
+        
+        print('')
+        print(prediction)
+        print('')
