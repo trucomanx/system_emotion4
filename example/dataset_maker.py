7faebb30cf3e2da2b0ad509bcc0c76acb95fa77c
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-09-17 17:44:33 -0300

    ordenando

--- example/dataset_maker.py
+++ example/dataset_maker.py
@@ -18,14 +18,31 @@ sys.path.append('../library')
 
 import SystemEmotion4Lib.tools_funcs as toolf
 
+import SystemEmotion4Lib.Classifier as sec
+cls=sec.Emotion4Classifier( checkpoint='shufflenetv2k16',
+                            model_type_face='efficientnet_b3',
+                            model_type_body='efficientnet_b3',
+                            model_type_skel=20,
+                            model_type_skel_enable_minus=True,
+                            model_type_fusion=7,
+                            body_factor=1.0,
+                            face_factor=0.85);
+
 def my_batch_func(batch):
-    import random
+    
+    img_pil_list=[];
+    for item in batch:
+        filepath=item[0];
+        img_pil_list.append(Image.open(filepath));
+
+    res=cls.from_img_pil_list(img_pil_list);
+    
     words = ['negative', 'neutro', 'pain', 'positive']
     
-    return [random.choice(words) for ID in range(len(batch)) ]
+    return [words[ID] for ID in res];
 
 
-DIRECTORY = '/home/fernando/Downloads/output/'
+DIRECTORY = '/home/fernando/Downloads/output'
 
 png_files = toolf.list_png_in_match_subdir(DIRECTORY,match_subdir='body',file_ext='.png');
 
@@ -35,9 +52,14 @@ if res:
     print("Todos os",len(file_list), "arquivos existem.");
     toolf.save_dataset_list_in_csv_batch(DIRECTORY,file_list, 'output.csv', my_batch_func);
 else:
-    print("Arquivos faltando encontrados:");
-    for files in file_list:
-        print(files);
+    print("Arquivos com defeito:");
+    with open('error.csv', 'w') as arquivo:
+        arquivo.write('body,face,skeleton\n')
+        for item in file_list:
+            print(item);
+            arquivo.write(item[0]+','+item[1]+','+item[2]+'\n')
+    print("Achados",len(file_list),"arquivos com defeito. Ver error.csv");
+    
     exit();
 
 
