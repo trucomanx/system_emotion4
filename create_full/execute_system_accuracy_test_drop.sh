cbf98afbbcd1e28ca51d17a575134205d30f9869
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-06-11 02:22:49 -0300

    ordenando

--- create_full/execute_system_accuracy_test_drop.sh
+++ create_full/execute_system_accuracy_test_drop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MachinePath='/mnt/8811f502-ae19-4dd8-8371-f1915178f581/Fernando'
+MachinePath='/media/fernando/Expansion'
 
-OutDir=$MachinePath"/OUTPUTS/DOCTORADO2/SYSTEM/system_emotion4_full"
+OutDir=$MachinePath"/OUTPUTS/DOCTORADO2/SYSTEM/system_emotion4_full_minus_drop_plus"
 
 ################################################################################
 export TF_USE_LEGACY_KERAS=1 
@@ -14,7 +14,7 @@ python3 system_accuracy.py  --model-type-check 'shufflenetv2k16' \
                             --model-type-face 'efficientnet_b3' \
                             --model-type-body 'efficientnet_b3' \
                             --model-type-skel 81 \
-                            --model-type-fusion 11 \
+                            --model-type-fusion 39 \
                             --dataset-dir $MachinePath"/DATASET/TESE-DROP-FACE10" \
                             --dataset-file "test_body.csv" \
                             --dataset-name "full2024-body-drop-face10" \
@@ -29,7 +29,7 @@ python3 system_accuracy.py  --model-type-check 'shufflenetv2k16' \
                             --model-type-face 'efficientnet_b3' \
                             --model-type-body 'efficientnet_b3' \
                             --model-type-skel 81 \
-                            --model-type-fusion 11 \
+                            --model-type-fusion 39 \
                             --dataset-dir $MachinePath"/DATASET/TESE" \
                             --dataset-file "test_body.csv" \
                             --dataset-name "full2024-body" \
